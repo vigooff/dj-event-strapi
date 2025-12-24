a268c38433319be3ec49d8357be8e9109c15a674
Vii <vgooffica@gmail.com>
2025-12-24 13:08:26 +0700

    insyaAllah final

--- config/database.ts
+++ config/database.ts
@@ -2,14 +2,10 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      connectionString: env('DATABASE_URL'),
       ssl: {
         rejectUnauthorized: false,
       },
     },
-    pool: { min: 2, max: 10 },
   },
 });
