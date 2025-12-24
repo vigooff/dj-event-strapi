9c2b8254959454128483a9bd2a6845e29e0f04fa
Vii <vgooffica@gmail.com>
2025-12-24 12:43:54 +0700

    test database.ts

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,10 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
+    debug: true,
   },
 });
