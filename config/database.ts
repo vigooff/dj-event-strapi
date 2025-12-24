51d853e1a10219f9eaa09758c585c21664660302
Vii <vgooffica@gmail.com>
2025-12-24 10:28:56 +0700

    final lagi

--- config/database.ts
+++ config/database.ts
@@ -3,14 +3,11 @@ export default ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 5432),
+      port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true)
-        ? { rejectUnauthorized: false }
-        : false,
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    pool: { min: 2, max: 10 },
   },
 });
